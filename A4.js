7078116024f3731b8aa17f325b719dfa9483e112
isabel <caralhocuodio@gmail.com>
2024-05-08 08:29:02 -0300

    aula 4, otimização

---
+++ A4.js
@@ -0,0 +1,17 @@
+const Teste = (n) => {
+    let calculo = n%2 
+    
+    if (calculo ==1) {
+        return "numero impar"
+    }
+    else {
+        return "numero par"
+    }
+}
+
+console.log(Teste(17))
+console.log(Teste(64)) 
+
+return (Teste ==1 ) ? "numero impar" : "numero par"
+
+console.log(Teste(19))
